9cfa86af51d3975d75451040a24aa787418e1be5
Steve Nolte <steve.nolte@corvisa.com>
2015-02-17 16:00:47 -0600

    Say hi to Ryan

--- src/main.lua
+++ src/main.lua
@@ -2,7 +2,7 @@
 -- coverage: 0
 
 channel.answer()
-channel.say("This is an example application. I like cats. Enter any number followed by the pound sign. Then eat a hotdog.")
+channel.say("This is an example application. Hi Ryan! I like cats. Enter any number followed by the pound sign. Then eat a hotdog.")
 local digits = channel.gather()
 channel.say(digits)
 channel.hangup()
