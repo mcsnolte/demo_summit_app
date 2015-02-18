398b9fe68f95db95d5c7000ede673c5385249459
Steve Nolte <steve.nolte@corvisa.com>
2015-02-18 09:29:49 -0600

    puzza

--- src/main.lua
+++ src/main.lua
@@ -2,8 +2,9 @@
 -- coverage: 0
 
 channel.answer()
-channel.say("Oh hello! What kind of pizza would you like to order?  For pepperoni, press 1.  For sausage, press 2.  For pepperoni and sausage, press 3.  For supreme, press 4.  For just cheese, stop and think if you really want just cheese. For all other pizzas, press 5.")
+channel.say("Oh hello! What kind of pizza would you like to order?  For pupperoni, press 1.  For snausage, press 2.  For pupperoni and snausage, press 3.  For soupreme, press 4.  For all other pizzas, press 5.")
 local digits = channel.gather()
+channel.say("Neat!  Thank you for your puzza rorder.  You ordered")
 channel.say(digits)
 channel.hangup()
 
